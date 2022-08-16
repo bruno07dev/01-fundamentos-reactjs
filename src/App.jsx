e33f990c25647a6fa4755fd08b6806a36ea37c61
Bruno Cardoso <brunomusic_3@hotmail.com>
2022-08-16 15:41:15 -0300

    creation and styling sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post';
+
+import styles from './App.module.css';
+
 import './global.css';
 
 export function App() {
@@ -7,10 +11,19 @@ export function App() {
     <div>
       <Header />
       
-      <Post 
-        author="Bruno Cardoso" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet alias dolores culpa voluptatem, suscipit, soluta nesciunt quod eveniet vero quidem, praesentium sit quibusdam officiis consectetur officia aut voluptatum expedita aperiam!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Bruno Cardoso" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet alias dolores culpa voluptatem, suscipit, soluta nesciunt quod eveniet vero quidem, praesentium sit quibusdam officiis consectetur officia aut voluptatum expedita aperiam!"
+          />
+          <Post 
+            author="Antônio Carlos" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae aut aperiam neque inventore harum ducimus accusamus, fugit accusantium placeat, adipisci magnam vitae veritatis voluptate eveniet ipsum corporis? Asperiores, aut?"
+          />
+        </main>
+      </div>
     </div>
   )
 }
