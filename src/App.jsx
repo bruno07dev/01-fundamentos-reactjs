9242964fe0f983d20861aadb9145aa773ca6607d
Bruno Cardoso <brunomusic_3@hotmail.com>
2022-08-18 20:49:20 -0300

    Structuring and integrating post component data for dynamic display of comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/bruno07dev.png',
+      name: 'BrunoCardoso',
+      role: 'Fullstack Developer Web and Mobile'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },        
+    ],
+    publishedAt: new Date('2022-08-16 19:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80',
+      name: 'Jhon Doe',
+      role: 'Software Enginer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },        
+    ],
+    publishedAt: new Date('2022-08-18 14:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,14 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Bruno Cardoso" 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet alias dolores culpa voluptatem, suscipit, soluta nesciunt quod eveniet vero quidem, praesentium sit quibusdam officiis consectetur officia aut voluptatum expedita aperiam!"
-          />
-          <Post 
-            author="Antônio Carlos" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae aut aperiam neque inventore harum ducimus accusamus, fugit accusantium placeat, adipisci magnam vitae veritatis voluptate eveniet ipsum corporis? Asperiores, aut?"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
